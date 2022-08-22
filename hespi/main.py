9c6222ac8844d6ad3da0f47e8f8e3c3f0da7c19e
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-08-22 22:58:53 +1000

    :zap: outputting fields from institutional labels

--- hespi/main.py
+++ hespi/main.py
@@ -6,11 +6,49 @@ from yolov5 import YOLOv5
 import tempfile
 from shutil import move
 from PIL import Image
-import numpy as np
+from collections import defaultdict
 
 console = Console()
 
 app = typer.Typer()
+
+def yolo_output(model, images, output_dir):
+    results = model.predict(images)
+    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_dir_path = Path(tmp_dir.name)
+    # TODO check that all images have unique names
+    
+    results.save( save_dir=tmp_dir_path )
+
+    output_files = defaultdict(list)
+
+    for image, predictions in zip(images, results.pred):
+        last_period = image.name.rfind(".")
+        stub = image.name[:last_period] if last_period else image.name
+        image_output_dir = output_dir/stub
+        image_output_dir.mkdir(exist_ok=True, parents=True)
+        prediction_path = image_output_dir/f"{stub}-predictions.jpg"
+        console.print(f"Saving sheet component predicitons with bounding boxes to '{prediction_path}'")
+        move(tmp_dir_path/f"{stub}.jpg", prediction_path)
+
+        for prediction_index, prediction in enumerate(predictions):
+            category_index = prediction[5].int().numpy()
+            category = results.names[category_index].replace(" ", "_").replace(":","").strip()
+
+            x0, y0, x1, y1 = prediction[:4].numpy()
+            
+            # open image
+            im = Image.open(image)
+            im_crop = im.crop((x0, y0, x1, y1))
+            output_filename = image_output_dir/f"{stub}.{prediction_index}.{category}.jpg"
+            console.print(f"Saving {category} to '{output_filename}'")
+            im_crop.save(output_filename)    
+            output_files[image].append(output_filename)
+    
+    tmp_dir.cleanup()
+
+    return output_files
+
 
 @app.command()
 def main(images:List[Path], output_dir:Path=Path("output")):
@@ -27,44 +65,61 @@ def main(images:List[Path], output_dir:Path=Path("output")):
 
     device = "cpu"
 
-    # Sheet-Component Detection Model
+    # Sheet-Components Detection Model
     sheet_component_weights_path = "sheet-component-weights.pt"
     sheet_component_model = YOLOv5(sheet_component_weights_path, device)
-    results = sheet_component_model.predict(images)
 
-    # Save predictions
-    tmp_dir = tempfile.TemporaryDirectory()
-    tmp_dir_path = Path(tmp_dir.name)
-    results.save( save_dir=tmp_dir_path )
+    # Institutional-Label-Fields Detection Model
+    institutional_label_fields_weights_path = "institutional-label-fields.pt"
+    institutional_label_fields_model = YOLOv5(institutional_label_fields_weights_path, device)
 
-    for image, predictions in zip(images, results.pred):
-        last_period = image.name.rfind(".")
-        stub = image.name[:last_period] if last_period else image.name
-        image_output_dir = output_dir/stub
-        image_output_dir.mkdir(exist_ok=True, parents=True)
-        prediction_path = image_output_dir/f"{stub}-predictions.jpg"
-        console.print(f"Saving predicitons with bounding boxes to '{prediction_path}'")
-        move(tmp_dir_path/f"{stub}.jpg", prediction_path)
+    # Sheet-Components predictions
+    component_files = yolo_output( sheet_component_model, images, output_dir=output_dir )
+    for stub, components in component_files.items():
+        for component in components:
+            if component.name.endswith("institutional_label.jpg"):
+                field_files = yolo_output( institutional_label_fields_model, [component], output_dir=output_dir/stub/"fields" )
 
-        breakpoint()
 
-        for prediction_index, prediction in enumerate(predictions):
-            category_index = prediction[5].int().numpy()
-            category = results.names[category_index].replace(" ", "_").replace(":","").strip()
+    # results = sheet_component_model.predict(images)
+    # tmp_dir = tempfile.TemporaryDirectory()
+    # tmp_dir_path = Path(tmp_dir.name)
+    # results.save( save_dir=tmp_dir_path )
 
-            x0, y0, x1, y1 = prediction[:4].numpy()
+    # for image, predictions in zip(images, results.pred):
+    #     last_period = image.name.rfind(".")
+    #     stub = image.name[:last_period] if last_period else image.name
+    #     image_output_dir = output_dir/stub
+    #     image_output_dir.mkdir(exist_ok=True, parents=True)
+    #     prediction_path = image_output_dir/f"{stub}-predictions.jpg"
+    #     console.print(f"Saving sheet component predicitons with bounding boxes to '{prediction_path}'")
+    #     move(tmp_dir_path/f"{stub}.jpg", prediction_path)
+
+    #     for prediction_index, prediction in enumerate(predictions):
+    #         category_index = prediction[5].int().numpy()
+    #         category = results.names[category_index].replace(" ", "_").replace(":","").strip()
+
+    #         x0, y0, x1, y1 = prediction[:4].numpy()
             
-            # open image
-            im = Image.open(image)
-            im_crop = im.crop((x0, y0, x1, y1))
-            output_filename = image_output_dir/f"{stub}.{prediction_index}.{category}.jpg"
-            im_crop.save(output_filename)
+    #         # open image
+    #         im = Image.open(image)
+    #         im_crop = im.crop((x0, y0, x1, y1))
+    #         output_filename = image_output_dir/f"{stub}.{prediction_index}.{category}.jpg"
+    #         console.print(f"Saving {category} to '{output_filename}'")
+    #         im_crop.save(output_filename)
 
-        # Institutional Label Classification
+    #         breakpoint()
 
-        # Institutional Label Field Detection Model
+    #         # Institutional Label Field Detection Model Predictions
+    #         if category == "institutional_label":
+    #             field_results = institutional_label_fields_model.predict(output_filename)
+    #             field_results.save( save_dir=tmp_dir_path )
+    #             field_predictions = field_results.pred[0]
 
-            # OCR/HTR
+    #             # TODO Institutional Label Classification
+    # for image, predictions in zip(images, results.pred):
 
-    tmp_dir.cleanup()
+    #             # OCR/HTR
+
+    # tmp_dir.cleanup()
     # Report
