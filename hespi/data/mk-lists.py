6ca3995dd74b1429b34cbcd36677f535a0fdcd42
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-09-07 17:15:53 +1000

    :art: black formatting

--- hespi/data/mk-lists.py
+++ hespi/data/mk-lists.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from pathlib import Path
 
+
 def clean_value(value):
     value = value.strip()
     if value.startswith("'") and value.endswith("'"):
         value = value[1:-1]
     return value
 
+
 work_dir = Path(__file__).parent
-df = pd.read_csv(work_dir/"plants.csv")
-for column in ['family', 'genus', 'species', 'authority']:
+df = pd.read_csv(work_dir / "plants.csv")
+for column in ["family", "genus", "species", "authority"]:
     values = df[~pd.isna(df[column])][column].unique()
     values = set(clean_value(value) for value in values)
     values = set(value for value in values if value)
     values = sorted(values)
-    output_path = work_dir/f"{column}.txt"
+    output_path = work_dir / f"{column}.txt"
     print(output_path)
     output_path.write_text("\n".join(values))
--- hespi/main.py
+++ hespi/main.py
@@ -1,26 +1,23 @@
-from typing import List 
+from typing import List
 import typer
 from pathlib import Path
 import pandas as pd
-
 from difflib import get_close_matches
 import torch
 from yolov5 import YOLOv5
 from torchapp.examples.image_classifier import ImageClassifier
-import pytesseract
-
 from rich.console import Console
 
 from .yolo import yolo_output
 from .ocr import Tesseract, TrOCR, TrOCRSize
 
-
 console = Console()
 
 app = typer.Typer()
 
+
 def adjust_case(field, value):
-    if field in ['genus', 'family']:
+    if field in ["genus", "family"]:
         return value.title()
     if field == "species":
         return value.lower()
@@ -28,23 +25,47 @@ def adjust_case(field, value):
 
 
 def read_reference(field):
-    DATA_DIR = Path(__file__).parent/"data"
-    path = DATA_DIR/f"{field}.txt"
+    DATA_DIR = Path(__file__).parent / "data"
+    path = DATA_DIR / f"{field}.txt"
     return path.read_text().split("\n")
 
 
 @app.command()
 def detect(
-    images:List[Path] = typer.Argument(..., help="A list of images to process."), 
-    output_dir:Path = typer.Option(..., help="A directory to output the results.", prompt="Please specify a directory for the results"),
-    gpu:bool = typer.Option(True, help="Whether or not to use a GPU if available."),
-    fuzzy:bool = typer.Option(True, help="Whether or not to use fuzzy matching from teh reference database."),
-    fuzzy_cutoff:float = typer.Option(0.8, min=0.0, max=1.0, help="The threshold for the fuzzy matching score to use."),
-    htr:bool = typer.Option(True, help="Whether or not to do handwritten text recognition using Microsoft's TrOCR."),
-    trocr_size:TrOCRSize = typer.Option(TrOCRSize.BASE.value, help="The size of the TrOCR model to use for handwritten text recognition.", case_sensitive=False),
-    sheet_component_weights:Path = typer.Option("sheet-component-weights.pt", help="The path to the sheet component model weights."),
-    institutional_label_fields_weights:Path = typer.Option("institutional-label-fields.pt", help="The path to the institutional label field model weights."),
-    institutional_label_classifier_weights:Path = typer.Option("institutional-label-classifier.pkl", help="The path to the institutional label classifier weights."),
+    images: List[Path] = typer.Argument(..., help="A list of images to process."),
+    output_dir: Path = typer.Option(
+        ...,
+        help="A directory to output the results.",
+        prompt="Please specify a directory for the results",
+    ),
+    gpu: bool = typer.Option(True, help="Whether or not to use a GPU if available."),
+    fuzzy: bool = typer.Option(
+        True, help="Whether or not to use fuzzy matching from teh reference database."
+    ),
+    fuzzy_cutoff: float = typer.Option(
+        0.8, min=0.0, max=1.0, help="The threshold for the fuzzy matching score to use."
+    ),
+    htr: bool = typer.Option(
+        True,
+        help="Whether or not to do handwritten text recognition using Microsoft's TrOCR.",
+    ),
+    trocr_size: TrOCRSize = typer.Option(
+        TrOCRSize.BASE.value,
+        help="The size of the TrOCR model to use for handwritten text recognition.",
+        case_sensitive=False,
+    ),
+    sheet_component_weights: Path = typer.Option(
+        "sheet-component-weights.pt",
+        help="The path to the sheet component model weights.",
+    ),
+    institutional_label_fields_weights: Path = typer.Option(
+        "institutional-label-fields.pt",
+        help="The path to the institutional label field model weights.",
+    ),
+    institutional_label_classifier_weights: Path = typer.Option(
+        "institutional-label-classifier.pkl",
+        help="The path to the institutional label classifier weights.",
+    ),
 ):
     """
     HErbarium Specimen sheet PIpeline
@@ -68,21 +89,29 @@ def detect(
 
     # Sheet-Components Detection Model
     if not sheet_component_weights.exists():
-        raise FileNotFoundError(f"Cannot find the Sheet-Components model weights at path '{sheet_component_weights}.'")
+        raise FileNotFoundError(
+            f"Cannot find the Sheet-Components model weights at path '{sheet_component_weights}.'"
+        )
     sheet_component_model = YOLOv5(sheet_component_weights, device)
 
     # Institutional-Label-Fields Detection Model
     if not institutional_label_fields_weights.exists():
-        raise FileNotFoundError(f"Cannot find the Institutional-Label-Fields model weights at path '{institutional_label_fields_weights}.'")
-    institutional_label_fields_model = YOLOv5(institutional_label_fields_weights, device)
+        raise FileNotFoundError(
+            f"Cannot find the Institutional-Label-Fields model weights at path '{institutional_label_fields_weights}.'"
+        )
+    institutional_label_fields_model = YOLOv5(
+        institutional_label_fields_weights, device
+    )
 
     # ImageClassifier
     if not institutional_label_classifier_weights.exists():
-        raise FileNotFoundError(f"Cannot find the Institutional-Label-Classifier model weights at path '{institutional_label_classifier_weights}.'")
+        raise FileNotFoundError(
+            f"Cannot find the Institutional-Label-Classifier model weights at path '{institutional_label_classifier_weights}.'"
+        )
     institutional_label_classifier = ImageClassifier()
 
-    reference_fields = ['family', 'genus', 'species', 'authority']
-    reference = {field:read_reference(field) for field in reference_fields}
+    reference_fields = ["family", "genus", "species", "authority"]
+    reference = {field: read_reference(field) for field in reference_fields}
 
     # OCR models
     tesseract = Tesseract()
@@ -92,38 +121,48 @@ def detect(
         print("TrOCR model available")
 
     # Sheet-Components predictions
-    component_files = yolo_output( sheet_component_model, images, output_dir=output_dir )
+    component_files = yolo_output(sheet_component_model, images, output_dir=output_dir)
 
     ocr_data = {}
     # Institutional Label Field Detection Model Predictions
     for stub, components in component_files.items():
         for component in components:
             if component.name.endswith("institutional_label.jpg"):
-                field_files = yolo_output( institutional_label_fields_model, [component], output_dir=output_dir/stub )
+                field_files = yolo_output(
+                    institutional_label_fields_model,
+                    [component],
+                    output_dir=output_dir / stub,
+                )
                 row = {
-                    "id":stub,
+                    "id": stub,
                 }
 
                 # Institutional Label Classification
-                classification_csv = component.parent/f"{component.name[:3]}.classification.csv"
+                classification_csv = (
+                    component.parent / f"{component.name[:3]}.classification.csv"
+                )
                 console.print(f"Classifying institution label: '{component}'")
                 console.print(f"Saving result to '{classification_csv}'")
                 classifier_results = institutional_label_classifier(
-                    items=component, 
-                    pretrained=institutional_label_classifier_weights, 
-                    gpu=gpu, 
+                    items=component,
+                    pretrained=institutional_label_classifier_weights,
+                    gpu=gpu,
                     output_csv=classification_csv,
                     verbose=False,
                 )
 
                 # Get classification of institution label
-                # 
+                #
                 if isinstance(classifier_results, pd.DataFrame):
-                    classification = classifier_results.iloc[0]['prediction']
-                    row['label_classification'] = classification
-                    console.print(f"'{component}' classified as '[red]{classification}[/red]'.")
+                    classification = classifier_results.iloc[0]["prediction"]
+                    row["label_classification"] = classification
+                    console.print(
+                        f"'{component}' classified as '[red]{classification}[/red]'."
+                    )
                 else:
-                    console.print(f"Could not get classification of institutional label '{component}'")
+                    console.print(
+                        f"Could not get classification of institutional label '{component}'"
+                    )
                     classification = None
 
                 # Text Recognition on bounding boxes found by Yolo
@@ -140,7 +179,9 @@ def detect(
                             htr_text = trocr.get_text(field_file)
                             if htr_text:
                                 print(f"HTR: {htr_text}")
-                                console.print(f"Handwritten Text Recognition (TrOCR): [red]'{htr_text}'[/red]")
+                                console.print(
+                                    f"Handwritten Text Recognition (TrOCR): [red]'{htr_text}'[/red]"
+                                )
 
                                 row[f"{field}_TrOCR"] = htr_text
                                 recognised_text = htr_text
@@ -149,11 +190,15 @@ def detect(
                         tesseract_text = tesseract.get_text(field_file)
                         if tesseract_text:
                             row[f"{field}_tesseract"] = tesseract_text
-                            console.print(f"Optical Character Recognition (Tesseract): [red]'{tesseract_text}'[/red]")
-                            
-                            if (classification and classification in ["printed", "typewriter"]) or not recognised_text:
-                                recognised_text = tesseract_text
+                            console.print(
+                                f"Optical Character Recognition (Tesseract): [red]'{tesseract_text}'[/red]"
+                            )
 
+                            if (
+                                classification
+                                and classification in ["printed", "typewriter"]
+                            ) or not recognised_text:
+                                recognised_text = tesseract_text
 
                         # Adjust text if necessary
                         if recognised_text:
@@ -162,40 +207,59 @@ def detect(
 
                             # Match with database
                             if fuzzy and field in reference:
-                                close_matches = get_close_matches(text_adjusted, reference[field], cutoff=fuzzy_cutoff, n=1)
+                                close_matches = get_close_matches(
+                                    text_adjusted,
+                                    reference[field],
+                                    cutoff=fuzzy_cutoff,
+                                    n=1,
+                                )
                                 if close_matches:
                                     text_adjusted = close_matches[0]
 
                             if recognised_text != text_adjusted:
-                                console.print(f"Recognized text [red]'{recognised_text}'[/red] adjusted to [purple]'{text_adjusted}'[/purple]")
+                                console.print(
+                                    f"Recognized text [red]'{recognised_text}'[/red] adjusted to [purple]'{text_adjusted}'[/purple]"
+                                )
 
                             row[field] = text_adjusted
-                
+
                 ocr_data[str(component)] = row
 
     csv_creation(ocr_data, output_dir)
 
 
-
-def csv_creation(data:dict, output_dir:Path):
+def csv_creation(data: dict, output_dir: Path):
     """
     Creates a DataFrame of data, sorts columns and outputs a CSV with OCR values.
     """
-    df = pd.DataFrame.from_dict(data, orient='index')
+    df = pd.DataFrame.from_dict(data, orient="index")
     df = df.reset_index().rename(columns={"index": "institutional label"})
 
     # insert columns not included in dataframe, and re-order
     # including any columns not included in col_options to account for any updates
-    col_options = ['institutional label', "id", 'family', 'genus', 'species', 'infrasp taxon', 
-                    'authority', 'collector_number', 'collector', 
-                    'locality', 'geolocation', 'year', 'month', 'day']
+    col_options = [
+        "institutional label",
+        "id",
+        "family",
+        "genus",
+        "species",
+        "infrasp taxon",
+        "authority",
+        "collector_number",
+        "collector",
+        "locality",
+        "geolocation",
+        "year",
+        "month",
+        "day",
+    ]
 
     missing_cols = [col for col in col_options if col not in df.columns]
-    df[missing_cols] = ''
+    df[missing_cols] = ""
     extra_cols = [col for col in df.columns if col not in col_options]
     cols = col_options + extra_cols
     df = df[cols]
 
     # CSV output
-    df.to_csv(str(output_dir)+'/ocr_results.csv', index=False) 
+    df.to_csv(str(output_dir) + "/ocr_results.csv", index=False)
     return df
