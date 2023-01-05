a1b630d63c19ba8e64fdebb14c370927acbae475
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-01-05 17:18:23 +1100

    :cop: adding tests for tesseract

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,5 +1,7 @@
-from hespi import ocr
 import pytest
+from unittest.mock import patch
+
+from hespi import ocr
 
 def test_trocrsize():
     assert str(ocr.TrOCRSize.SMALL) == "small"
@@ -10,3 +12,22 @@ def test_trocrsize():
 def test_abstract_class():
     with pytest.raises(NotImplementedError):
         ocr.OCR().get_text("")
+
+@patch('pytesseract.image_to_string', return_value="recognized text")
+def test_tesseract(mock_image_to_string):
+    t = ocr.Tesseract()
+    assert t.no_tesseract == False
+    assert t.get_text('path') == "recognized text"
+    assert t.no_tesseract == False
+
+
+def raise_exception(*args):
+    raise Exception("error")
+
+
+@patch('pytesseract.image_to_string', raise_exception)
+def test_tesseract_fail():
+    t = ocr.Tesseract()
+    assert t.no_tesseract == False
+    assert t.get_text('path') is None
+    assert t.no_tesseract == True
