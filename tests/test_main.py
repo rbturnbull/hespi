7fccc71c6a286d0bd872486526e8aade513249f7
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-01-05 15:56:09 +1100

    :abc: adding test_adjust_case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,17 @@
 from typer.testing import CliRunner
 
-from hespi.main import app
+from hespi import main
 
 runner = CliRunner()
 
 
 def test_help():
-    result = runner.invoke(app, ["--help"])
+    result = runner.invoke(main.app, ["--help"])
     assert result.exit_code == 0
     assert "output" in result.stdout
 
 
+def test_adjust_case():
+    assert "Acanthochlamydaceae" == main.adjust_case("family", "ACANTHOCHLAMYDACEAE")
+    assert "Abutilon" == main.adjust_case("genus", "abutilon")
+    assert "zostericolum" == main.adjust_case("species", "Zostericolum")    
