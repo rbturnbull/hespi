606ae73e27ae8aaac488b3dd111a804d955345aa
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-03-03 00:00:42 +1100

    :tv: removing old yolov5

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,7 +16,6 @@ def test_help():
 
 @patch.object(Hespi, 'detect')
 def test_detect_command(mock_detect):
-    # breakpoint()/
     runner.invoke(main.app, ["image1.tif", "image2.jpg", "--output-dir", "output"])
 
     mock_detect.assert_called_once_with([Path("image1.tif"), Path("image2.jpg")], Path("output"))
